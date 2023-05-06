cfa8ec4f0056d12446ee31ae4b4b51b4c4d311c4
Allan Simbajon <asimbajon1@buffs.wtamu.edu>
2023-05-05 18:30:49 -0700

    Ready for grading

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -11,8 +11,8 @@ def test_happy_path_returns_202_and_batch_is_allocated():
     earlybatch = random_batchref(1)
     laterbatch = random_batchref(2)
     otherbatch = random_batchref(3)
-    api_client.post_to_add_batch(laterbatch, sku, 100, "2011-01-02")
-    api_client.post_to_add_batch(earlybatch, sku, 100, "2011-01-01")
+    api_client.post_to_add_batch(laterbatch, sku, 100, "2023-04-02")
+    api_client.post_to_add_batch(earlybatch, sku, 100, "2023-04-01")
     api_client.post_to_add_batch(otherbatch, othersku, 100, None)
 
     r = api_client.post_to_allocate(orderid, sku, qty=3)
